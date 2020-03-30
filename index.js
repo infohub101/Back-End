b042b30fc85cf7b9d2c8c57fb07ea9f59b241d95
RichVI <richardwang2050@gmail.com>
2020-03-29 21:12:37 -0700

    Sign Up & Login EndPoint complete

---
+++ index.js
@@ -0,0 +1,6 @@
+const server = require('./api/server');
+
+const PORT = process.env.PORT || 5000;
+server.listen(PORT, () => {
+    console.log(`\n** Infohub101 server is running on port ${PORT} **\n`);
+});
