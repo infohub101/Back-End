52c5c0d7b64e26ccbf10aca068a2a5fe0ec681d5
RichVI <richardwang2050@gmail.com>
2020-04-01 20:56:16 -0700

    PUT & DELETE endpoint added for user

--- database/seeds/01-apidatabase.js
+++ database/seeds/01-apidatabase.js
@@ -1,28 +1,29 @@
-const bcrypt = require('bcryptjs');
 exports.seed = function(knex) {
   // Deletes ALL existing entries
   return knex('apidatabase').truncate()
     .then(function () {
       // Inserts seed entries
-      return knex('apidatabase').insert([
-        {
-          user_id: 1, 
-          title: 'Title',
-          img: 'http://img.google.com',
-          url: 'https://newsapi.org/v2/top-headlines?country=us&apiKey=17bde5c1903e4a31a9d1560bf1256a95',
-        },
-        {
-          user_id: 2, 
-          title: 'Title',
-          img: 'http://img.google.com',
-          url: 'https://newsapi.org/v2/top-headlines?country=us&apiKey=17bde5c1903e4a31a9d1560bf1256a95',
-        },
-        {
-          user_id: 3, 
-          title: 'Title',
-          img: 'http://img.google.com',
-          url: 'https://newsapi.org/v2/sources?language=en&country=us&apiKey=17bde5c1903e4a31a9d1560bf1256a95',
-        },
-      ]);
+      return knex('apidatabase').insert(
+        [
+          {
+            user_id: 1, 
+            title: 'Title',
+            img: 'http://img.google.com',
+            url: 'https://newsapi.org/v2/top-headlines?country=us&apiKey=17bde5c1903e4a31a9d1560bf1256a95',
+          },
+          {
+            user_id: 2, 
+            title: 'Title',
+            img: 'http://img.google.com',
+            url: 'https://newsapi.org/v2/top-headlines?country=us&apiKey=17bde5c1903e4a31a9d1560bf1256a95',
+          },
+          {
+            user_id: 3, 
+            title: 'Title',
+            img: 'http://img.google.com',
+            url: 'https://newsapi.org/v2/sources?language=en&country=us&apiKey=17bde5c1903e4a31a9d1560bf1256a95',
+          },
+        ]
+      );
     });
 };
--- database/userdatabase/user-model.js
+++ database/userdatabase/user-model.js
@@ -16,28 +16,24 @@ function findById(id) {
     .where({id})
     .first();
 }
-// //Insert new user to db
-// async function insert(post) {
-//     return await db('posts').insert(post).returning(['id', 'title']);
-//   }
-// //Remove User
-// function remove(id){
-//     return db('posts')
-//     .where({id})
-//     .del(); //deletes the record that matches the id
-// }
-// //Update User
-// function update(changes, id){
-//     return db('posts')
-//     .where({id})
-//     .update(changes); //updates the record with 'changes' where the id matches
-// }
+
+//Remove User
+function remove(id){
+    return db('users')
+    .where({id})
+    .del(); //deletes the record that matches the id
+}
+//Update User
+function update(changes, id){
+    return db('users')
+    .where({id})
+    .update(changes); //updates the record with 'changes' where the id matches
+}
 
 module.exports ={
     find,
     findBy,
     findById,
-    // insert,
-    // remove,
-    // update
+    remove,
+    update
 }
