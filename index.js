fd200674162307f25215e3f5a9f50fc46728d2b1
RichVI <richardwang2050@gmail.com>
2020-04-10 12:04:13 -0700

    env testing

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
-require('dotenv').config();
-
 const server = require('./api/server');
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => {
     console.log(`\n** Infohub101 server is running on port ${PORT} **\n`);
 });
