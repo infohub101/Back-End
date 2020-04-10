b7b0e99b1d23a4ae9f1c494e1e115f3903ac0075
RichVI <richardwang2050@gmail.com>
2020-04-10 11:58:49 -0700

    env testing

--- database/migrations/20200410114836_users.js
+++ database/migrations/20200410114836_users.js
@@ -12,3 +12,5 @@ exports.up = function(knex) {
 exports.down = function(knex) {
     return knex.schema.dropTableIfExists('users');
 };
+
+
--- knexfile.js
+++ knexfile.js
@@ -1,35 +1,49 @@
-require("dotenv").config(); //access to .env variables
+// Update with your config settings.
 
-const database = {
-  client: 'pg',
-      connection: {
-          host: 'localhost',
-          database: 'dev'
-      },
-      migrations: {
-          directory: './database/migrations'
-      },
-      seeds: { 
-          directory: './database/seeds' 
-      },
-  };
+module.exports = {
 
-  module.exports = {
-
-    development:{
-      ...database,
-      connection: {
-        port: process.env.POSTGRES_PORT,
-        database: 'dev',
-        user: process.env.USER,
-        password: process.env.PASSWORD
-      },
-      tableName: 'knex_migrations'
+  development: {
+    client: 'sqlite3',
+    connection: {
+      filename: './database/auth.db3'
     },
+    useNullAsDefault: true,
+    migrations: {
+      directory: './database/migrations',
+    },
+    seeds: { directory: './database/seeds' },
+  },
 
-    production: {
-      ...database,
-      connection: process.env.DATABASE_URL,
+  staging: {
+    client: 'postgresql',
+    connection: {
+      database: 'my_db',
+      user:     'username',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
       tableName: 'knex_migrations'
     }
-  };
+  },
+
+  production: {
+    client: 'postgresql',
+    connection: {
+      database: 'my_db',
+      user:     'username',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  }
+
+};
