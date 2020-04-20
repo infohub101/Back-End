1df2a2696382b36f6eceebb7a80ef0084a252745
Dreamer <rwang24@student.cccd.edu>
2020-04-20 11:19:18 -0700

    MovieDB API added

--- database/seeds/01-apidatabase.js
+++ database/seeds/01-apidatabase.js
@@ -27,6 +27,26 @@ exports.seed = function(knex) {
             url: `https://api.nasa.gov/planetary/apod?api_key=${process.env.NasaAPI}`,
           },
 
+           // Entertainment 
+           {
+            category: "Entertainment",
+            api_id: "E1",
+            title: "The Movie DB",
+            description: "Get a list of upcoming movies in the US theatres. ",
+            img: "https://f0.pngfuel.com/png/815/325/red-and-white-popcorn-container-and-movie-tickets-clip-art-png-clip-art-thumbnail.png",
+            url: "https://api.themoviedb.org/3/movie/upcoming?api_key=2989523a56793797d69a0424d54817de&language=en-US&page=1&region=US",
+          },
+          
+           // Gambling 
+          {
+            category: "Gambling",
+            api_id: "G1",
+            title: "Powerball Ticket",
+            description: "Powerball Ticket retrieves the latest Powerball winning numbers",
+            img: "https://s3.amazonaws.com/cdn.powerball.com/drupal/files/powerball-power-play-68px.png",
+            url: "https://games.api.lottery.com/api/v2.0/results?game=59bc2b6031947b9daf338d32",
+          },
+
           // News
           {
             category: "News",
@@ -35,16 +55,6 @@ exports.seed = function(knex) {
             description: 'Daily US Headlines',
             img: 'https://i1.wp.com/www.madeiraislandnews.com/wp-content/uploads/2016/11/onslaatstenieuws.jpg?resize=759%2C343&ssl=1',
             url: `https://newsapi.org/v2/top-headlines?country=us&apiKey=${process.env.USNewsAPI}`,
-          },
-
-           // Entertainment 
-           {
-            category: "Entertainment",
-            api_id: "E1",
-            title: "Powerball Ticket",
-            description: "Powerball Ticket retrieves the latest Powerball winning numbers",
-            img: "https://s3.amazonaws.com/cdn.powerball.com/drupal/files/powerball-power-play-68px.png",
-            url: "https://games.api.lottery.com/api/v2.0/results?game=59bc2b6031947b9daf338d32",
           }
         ]
       );
